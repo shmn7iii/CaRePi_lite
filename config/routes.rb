76fc9d7102ca2de029c6abb2b14526099f10d27f
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-06-23 10:02:20 +0900

    update: set root path as users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'users#index'
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'sessions/new'
 end
