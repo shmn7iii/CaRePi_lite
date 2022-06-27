ffea936b8876aaff509049e3ad2d290efcaea3cc
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-06-27 09:37:45 +0900

    fix: typo on config/schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, 'log/crontab.log'
 ENV.each { |k, v| env(k, v) }
 rails_env = ENV['RAILS_ENV'] || :development
-set :environment, :rails_env
+set :environment, rails_env
 
 every '0 15 * * *' do
   rake 'carepi:leaveall'
